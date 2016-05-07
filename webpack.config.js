a0057000c2136b22e703236b05941d9116f4f1b1
andela-rekemezie <rowland.ekemezie@andela.com>
2016-05-07 05:24:01 +0100

    Initial setup, webpack configurations and linting

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const htmlWebpackPluginConfig = new htmlWebpackPlugin({
+  template: path.join(__dirname, '/app/index.html'),
+  inject: 'body',
+  filname: 'index.html'
+});
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules | bower_components)/,
+      loader: 'babel-loader'
+    },
+    { test: /\.css$/, loader: 'style-loader!css-loader' }
+  ]
+  },
+  plugins: [htmlWebpackPluginConfig]
+};
